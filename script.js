e7153377833fcc743d2239fb865817452724009e
abrahan <abrahandev@gmail.com>
2023-09-03 12:14:38 +0200

    working on scripts

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 
-pw
-Copy
-len
-upper
-lower
-number
-symbol
-generate
+const pw = document.getElementById('pw')
+const len = document.getElementById('len')
+const upperEl = document.getElementById('upper')
+const lowerEl = document.getElementById('lower')
+const numberEl = document.getElementById('number')
+const symbolEl = document.getElementById('symbol')
+const generateEl = document.getElementById('generate')
+
+const upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const lowerLetters = 'abcdefghijklmnopqrstuvwxyz'
+const numbers = '1234567890'
+const symbols = '!@#$%^&*()_-=+'
+
+function getLowerCase () {
+
+}
+
+function getUpperCase () {
+    
+}
+function getNumber () {
+    
+}
+function getSymbol () {
+    
+}
